622841bc363c5f9b0ccd23f9ac996311ca0e9abd
evelan <evelan3094@gmail.com>
2017-08-29 23:31:04 +0200

    Add content type header

--- src/main/java/me/jpomykala/yahoo/controller/FormatterController.java
+++ src/main/java/me/jpomykala/yahoo/controller/FormatterController.java
@@ -6,6 +6,7 @@ import me.jpomykala.yahoo.formatter.reader.ExcelReader;
 import me.jpomykala.yahoo.formatter.reader.TranslationsReader;
 import me.jpomykala.yahoo.formatter.writer.JsonWriter;
 import me.jpomykala.yahoo.formatter.writer.TranslationsWriter;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +20,7 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/api")
 public class FormatterController {
 
-	@PostMapping(value = "/format")
+	@PostMapping(value = "/format", produces = MediaType.APPLICATION_JSON_VALUE)
 	public byte[] formatFile(@RequestParam("file") MultipartFile file) throws IOException {
 		byte[] excelFile = file.getBytes();
 		TranslationsReader translationsReader = new ExcelReader();
