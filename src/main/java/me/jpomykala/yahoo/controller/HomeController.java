05b00ee96544c81634a52adb1bfb080488799826
evelan <evelan3094@gmail.com>
2017-08-31 00:29:25 +0200

    Add file name header

--- src/main/java/me/jpomykala/yahoo/controller/HomeController.java
+++ src/main/java/me/jpomykala/yahoo/controller/HomeController.java
@@ -1,7 +1,20 @@
 package me.jpomykala.yahoo.controller;
 
+import java.io.IOException;
+import java.util.Map;
+import javax.servlet.http.HttpServletResponse;
+import me.jpomykala.yahoo.formatter.reader.ExcelReader;
+import me.jpomykala.yahoo.formatter.reader.TranslationsReader;
+import me.jpomykala.yahoo.formatter.writer.JsonWriter;
+import me.jpomykala.yahoo.formatter.writer.TranslationsWriter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * @author Jakub Pomykala on 8/29/17.
@@ -11,8 +24,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
 	@RequestMapping(value = {"/", ""})
-	public String redirectToSwagger() {
-		return "redirect:/swagger-ui.html";
+	public String home() {
+		return "index";
+	}
+
+	@PostMapping(value = "/format", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	@ResponseBody
+	public byte[] formatFile(
+			HttpServletResponse response,
+			@RequestParam("file") MultipartFile file) throws IOException {
+		byte[] excelFile = file.getBytes();
+		TranslationsReader translationsReader = new ExcelReader();
+		Map<String, Map<String, String>> translationsMap = translationsReader.read(excelFile);
+		TranslationsWriter translationsWriter = new JsonWriter();
+		response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + "data.json");
+		return translationsWriter.write(translationsMap);
 	}
 
 }
